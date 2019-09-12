5c3c75507297b3ae2ac4fffd231564d9af146d9c
John Reyes <john.jreye@gmail.com>
2019-09-12 16:37:10 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(description, noise = "Squawk")
+ puts "A parrot is a #{description} that makes a #{noise} sound."
+ return "Squawk"
+ end
+
+ parrot("Pretty bird")
